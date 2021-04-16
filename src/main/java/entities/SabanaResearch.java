0ce43e51435413550f445ccaa319d285050ff9e3
Santiago Arrieta <santiagoarve@unisabana.edu.co>
2021-04-16 18:22:37 -0500

    me va a llevar el P

--- src/main/java/entities/SabanaResearch.java
+++ src/main/java/entities/SabanaResearch.java
@@ -5,13 +5,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SabanaResearch {
-
     private List<Group> groups;
     private List<Summary> summaries;
+    private List<Course> courses;
+    private List<Student> students;
 
     public SabanaResearch(List<Group> groups) {
         this.groups = groups;
-        this.summaries = new ArrayList<>();
+        this.summaries = new ArrayList();
+        this.courses = new ArrayList();
+        this.students = new ArrayList();
     }
 
     public int countOfGroups() {
@@ -22,13 +25,110 @@ public class SabanaResearch {
         return this.summaries.size();
     }
 
-    /**
-     * Create a summary entry in the current date.
-     * - Calculate the count of active projects.
-     *
-     * @return The new Summary entry.
-     */
+    public int countOfStudents() {
+        return this.students.size();
+    }
+
+    public int countOfCourses() {
+        return this.courses.size();
+    }
+
     public Summary createSummaryEntry() {
-        return null;
+        LocalDate date = LocalDate.now();
+        int activeProjects = this.countActiveProjects();
+        int openActivities = this.countOpenActivities();
+        int closedActivities = this.countClosedActivities();
+        return new Summary(activeProjects, date, openActivities, closedActivities);
+    }
+
+    public int countActiveProjects() {
+        int counter = 0;
+
+        for(int i = 0; i < this.groups.size(); ++i) {
+            for(int j = 0; j < ((Group)this.groups.get(j)).getProjects().size(); ++j) {
+                Project p = (Project)((Group)this.groups.get(j)).getProjects().get(j);
+                if (p.isActive()) {
+                    ++counter;
+                }
+            }
+        }
+
+        return counter;
+    }
+
+    public int countOpenActivities() {
+        int counter = 0;
+
+        for(int i = 0; i < this.groups.size(); ++i) {
+            for(int j = 0; j < ((Group)this.groups.get(j)).getProjects().size(); ++j) {
+                Project p = (Project)((Group)this.groups.get(j)).getProjects().get(j);
+                if (p.isActive()) {
+                    for(int k = 0; k < p.getIterations().size(); ++k) {
+                        for(int n = 0; n < ((Iteration)p.getIterations().get(k)).getActivities().size(); ++n) {
+                            Activity a = (Activity)((Iteration)p.getIterations().get(k)).getActivities().get(n);
+                            if (a.isActive()) {
+                                ++counter;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return counter;
+    }
+
+    public int countClosedActivities() {
+        int counter = 0;
+
+        for(int i = 0; i < this.groups.size(); ++i) {
+            for(int j = 0; j < ((Group)this.groups.get(j)).getProjects().size(); ++j) {
+                Project p = (Project)((Group)this.groups.get(j)).getProjects().get(j);
+                if (p.isActive()) {
+                    for(int k = 0; k < p.getIterations().size(); ++k) {
+                        for(int n = 0; n < ((Iteration)p.getIterations().get(k)).getActivities().size(); ++n) {
+                            Activity a = (Activity)((Iteration)p.getIterations().get(k)).getActivities().get(n);
+                            if (!a.isActive()) {
+                                ++counter;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return counter;
+    }
+
+    public List<Group> getGroups() {
+        return this.groups;
+    }
+
+    public void setGroups(List<Group> groups) {
+        this.groups = groups;
+    }
+
+    public List<Summary> getSummaries() {
+        return this.summaries;
+    }
+
+    public void setSummaries(List<Summary> summaries) {
+        this.summaries = summaries;
+    }
+
+    public List<Course> getCourses() {
+        return this.courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    public List<Student> getStudents() {
+        return this.students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
     }
 }
--- src/main/java/entities/Summary.java
+++ src/main/java/entities/Summary.java
@@ -3,16 +3,47 @@ package entities;
 import java.time.LocalDate;
 
 public class Summary {
-
     private int activeProjects;
     private LocalDate date;
+    private int openActivities;
+    private int closedActivities;
 
+    public Summary(int activeProjects, LocalDate date, int openActivities, int closedActivities) {
+        this.activeProjects = activeProjects;
+        this.date = date;
+        this.openActivities = openActivities;
+        this.closedActivities = closedActivities;
+    }
+
+    public void setActiveProjects(int activeProjects) {
+        this.activeProjects = activeProjects;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public int getOpenActivities() {
+        return this.openActivities;
+    }
+
+    public void setOpenActivities(int openActivities) {
+        this.openActivities = openActivities;
+    }
+
+    public int getClosedActivities() {
+        return this.closedActivities;
+    }
+
+    public void setClosedActivities(int closedActivities) {
+        this.closedActivities = closedActivities;
+    }
 
     public int getActiveProjects() {
-        return activeProjects;
+        return this.activeProjects;
     }
 
     public LocalDate getDate() {
-        return date;
+        return this.date;
     }
 }
